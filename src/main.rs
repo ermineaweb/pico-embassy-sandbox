0740b0237fa2e5743c2dd1bb47dd288c3dcb55f7
romain <romain@mail.com>
2024-12-29 22:11:59 +0100

    release opti

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-//! This example test the RP Pico on board LED.
-//!
-//! It does not work with the RP Pico W board. See wifi_blinky.rs.
-
 #![no_std]
 #![no_main]
 
@@ -15,15 +11,15 @@ use {defmt_rtt as _, panic_probe as _};
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
-    let mut led = Output::new(p.PIN_25, Level::Low);
+    let mut led = Output::new(p.PIN_1, Level::Low);
 
     loop {
         info!("led on!");
         led.set_high();
-        Timer::after_secs(1).await;
+        Timer::after_millis(500).await;
 
         info!("led off!");
         led.set_low();
-        Timer::after_secs(1).await;
+        Timer::after_millis(500).await;
     }
 }
